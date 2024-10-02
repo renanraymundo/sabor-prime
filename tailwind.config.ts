e3eae8926b097e796f4780873a3f558bca3863eb
Johnny Silva <jhsilva>
2024-10-02 15:46:08 -0300

    feat: add construction page
    This commit adds a temporary build page until the site is finished.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'bebas-neue': 'var(--font-bebas-neue)',
+        'dancing-script': 'var(--font-dancing-script)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        'sabor-prime': {
+          green: {
+            300: '#91D685',
+            500: '#65915C',
+          },
+          orange: {
+            100: '#FFCD74',
+            300: '#D4942C',
+          },
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
