87e31fb9f9d8587da8f98c5deac3ed38671d5d61
Johnny Silva <jhsilva>
2024-11-07 18:27:18 -0300

    Providers and NuxtUI theme adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       />
 
       <div className="space-y-3">
-        <h1 className="relative mx-auto flex max-w-max items-center gap-2 rounded-xl bg-gradient-to-r from-sabor-prime-orange-300 to-sabor-prime-orange-100 px-8 py-2 text-5xl text-white before:h-2 before:w-2 before:rounded-full before:bg-white after:h-2 after:w-2 after:rounded-full after:bg-white">
+        <h1 className="from-sabor-prime-orange-300 to-sabor-prime-orange-100 relative mx-auto flex max-w-max items-center gap-2 rounded-xl bg-gradient-to-r px-8 py-2 text-5xl text-white before:h-2 before:w-2 before:rounded-full before:bg-white after:h-2 after:w-2 after:rounded-full after:bg-white">
           Em Breve!
         </h1>
         <p className="text-center text-xl leading-6 text-slate-400">
